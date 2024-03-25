1cc229bc70e9cb1a02910ad3d858ee5ec3baef63
Wanni <wannidev0928@gmail.com>
2024-03-25 21:39:12 +0900

    project init

---
+++ src/main/kotlin/io/hhplus/lecture/HhplusLectureApplication.kt
@@ -0,0 +1,11 @@
+package io.hhplus.lecture
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HhplusLectureApplication
+
+fun main(args: Array<String>) {
+    runApplication<HhplusLectureApplication>(*args)
+}
---
+++ src/test/kotlin/io/hhplus/lecture/HhplusLectureApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.hhplus.lecture
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HhplusLectureApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
