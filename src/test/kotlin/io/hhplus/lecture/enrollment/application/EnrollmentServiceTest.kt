66e615b15c66113d02a4c25471c20ff42373ff01
Wanni <wannidev0928@gmail.com>
2024-03-26 02:01:04 +0900

    Test: 구현된 함수에 맞게 테스트 코드 재구성

--- src/test/kotlin/io/hhplus/lecture/enrollment/application/EnrollmentServiceTest.kt
+++ src/test/kotlin/io/hhplus/lecture/enrollment/application/EnrollmentServiceTest.kt
@@ -1,22 +1,53 @@
 package io.hhplus.lecture.enrollment.application
 
-import org.junit.jupiter.api.Assertions.*
+import io.hhplus.lecture.enrollment.domain.Enrollment
+import io.hhplus.lecture.enrollment.domain.EnrollmentRepository
+import io.hhplus.lecture.lesson.domain.Lesson
+import io.hhplus.lecture.lesson.domain.LessonRepository
+import io.hhplus.lecture.user.domain.User
+import io.hhplus.lecture.user.domain.UserRepository
 import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.junit.jupiter.MockitoExtension
+import org.mockito.kotlin.any
+import org.mockito.kotlin.doReturn
+import org.mockito.kotlin.mock
 import org.mockito.kotlin.verify
+import java.time.LocalDate
 import kotlin.test.Test
 
 @ExtendWith(MockitoExtension::class)
 class EnrollmentServiceTest {
-        @Test
+    /**
+     * 1. 수강 신청 api를 호출하면 enroll 메서드가 호출되는지 확인한다.
+     * 2. 선착순 인원 제한수를 넘지 않는지 확인한다.
+     * 3. 신청일자가 마감일자를 넘지 않는지 확인한다.
+     * 3. 제한수와 마감일자를 넘지 않으면, 성공
+     * 4. 제한수 혹은 마감일자를 넘으면, 실패
+     */
+    @Test
     fun `should enroll`() {
         // given
-        val enrollmentService = EnrollmentService()
+        val user = User()
+        user.id = 1
+        val lesson = Lesson(30, 0, LocalDate.of(2024, 4, 20))
+        lesson.id = 1
+        val enrollment = Enrollment.newInstance(user, lesson)
+        enrollment.id = 1
+        val mockUserRepository = mock<UserRepository> {
+            on { findById(any()) } doReturn user
+        }
+        val mockLessonRepository = mock<LessonRepository> {
+            on { findById(any()) } doReturn lesson
+        }
+        val mockEnrollmentRepository = mock<EnrollmentRepository> {
+            on { save(any()) } doReturn enrollment
+        }
+        val enrollmentService = EnrollmentService(mockUserRepository, mockLessonRepository, mockEnrollmentRepository)
 
         // when
-        enrollmentService.enroll()
+        enrollmentService.enroll(user.id!!, lesson.id!!)
 
         // then
-        verify(enrollmentService).enroll()
+        verify(mockEnrollmentRepository).save(any())
     }
 }
