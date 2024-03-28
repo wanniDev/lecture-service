40d5f23c81fa586f010b6d0fe4749c28eaa5471f
Wanni <wannidev0928@gmail.com>
2024-03-28 23:04:28 +0900

    Feat: 동시성 제어를 위해 강의 조회 코드에 비관적 락 적용
    innoDB의 mvvm 특성에 따라 팬텀리드 현상이 발생할 수 있다. 이를 방지하기위해, 쓰기가 진행되는 중에는 강의 조회 쿼리 트랜잭션을 막게하는 비관적 락을 적용한다.

--- src/main/kotlin/io/hhplus/lecture/lesson/infrastructure/jpa/JpaLessonRepository.kt
+++ src/main/kotlin/io/hhplus/lecture/lesson/infrastructure/jpa/JpaLessonRepository.kt
@@ -1,7 +1,11 @@
 package io.hhplus.lecture.lesson.infrastructure.jpa
 
 import io.hhplus.lecture.lesson.domain.Lesson
+import jakarta.persistence.LockModeType
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Lock
 
 interface JpaLessonRepository: JpaRepository<Lesson, Long> {
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    fun findLessonById(id: Long): Lesson
 }
